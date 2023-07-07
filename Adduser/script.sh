a963925db5fa43ddd5ce113c1b7671d54d3194b2
Nikunz <123695337+Nikunz@users.noreply.github.com>
2023-07-07 19:05:31 -0400

    Update script.sh

--- Adduser/script.sh
+++ Adduser/script.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Create a new user account
-useradd -m -s /bin/bash newuser
+useradd -m -s /bin/bash newuser1
 
 # Prompt for the user's password
-read -s -p "Enter password for newuser: " password
-echo "newuser:$password" | chpasswd
+read -s -p "Enter password for newuser1: " password
+echo "newuser1:$password" | chpasswd
 
 # Add the user to the sudo group
 usermod -aG sudo newuser\
 
-echo "New user account created successfully."
+echo "New user1 account created successfully."
