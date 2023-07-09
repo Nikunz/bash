00bee2fcf2547200d70a62f492c0c308bf64b802
Nikunz <123695337+Nikunz@users.noreply.github.com>
2023-07-09 18:10:10 -0400

    Create weather.sh

---
+++ weather/weather.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $1 in
+-h | --help)
+        echo "$program $version"
+        echo "Copyright $year $developer. All rights reserved."
+        echo
+        echo "Usage: weather [options]"
+        echo "Option          Long Option             Description"
+        echo "-h              --help                  Show the help screen"
+        echo "-l [location]   --location [location]   Specifies the location"
+        ;;
+-l | --location)
+        curl https://wttr.in/$2
+        ;;
+*)
+        curl https://wttr.in
+        ;;
+esac
